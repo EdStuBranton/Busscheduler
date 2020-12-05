46fcfcf37114e0863eab67c40886501a6cd429fe
EdStuBranton <60286194+EdStuBranton@users.noreply.github.com>
2020-12-05 20:25:11 +0100

    Create Busscheduler_main.py

---
+++ Busscheduler_main.py
@@ -0,0 +1,50 @@
+import time
+import datetime
+
+__author__ = "Ed Stu Branton"
+__copyright__ = "Copyright 2020 Ed Stu Branton"
+__credits__ = ["Ed Stu Branton"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Ed Stu Branton"
+__email__ = "edstubranton@hotmail.com"
+__status__ = "Production"
+
+"""
+Busschedule input as Dictionary
+"""
+busschedule_weekday = {'a':[8,25], 'b':[8,45],
+        'c':[9,25], 'd':[9,45],
+        'e':[10,25], 'f':[10,45],
+        'g':[11,25], 'h':[11,45],
+        'i':[12,25], 'j':[12,45],
+        'k':[13,25], 'l':[13,45],
+        'm':[14,25], 'n':[14,45],
+        'o':[15,25], 'p':[15,45],
+        'q':[16,25], 'r':[16,45],
+        's':[17,25], 't':[17,45],
+        'u':[18,25], 'v':[18,45]}
+
+
+
+def rtrn_curr_hr_min_split():    
+    current_hour, current_minute = time.strftime("%H:%M").split(':')
+    return current_hour, current_minute
+
+def normalize_hours(remaining_hours):
+    if remaining_hours < 0:
+        remaining_hours += 24    
+    return remaining_hours
+
+def normalize_minutes(remaining_minutes):
+    if remaining_minutes < 0:
+        remaining_minutes += 60        
+    return remaining_minutes
+
+
+def time_needed_to_busstation(m):
+    result = int((((m / 1000) / 5) * 3600))
+    minutes = int(result / 60)
+    seconds = result - (minutes * 60) 
+    
+    return '{0} Minute/s and {1} Second/s'.format(minutes, seconds)
